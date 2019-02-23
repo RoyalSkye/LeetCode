604c920d76b2f0987012021dad79494d64491bff
royalskye <a19970417b@qq.com>
2019-02-23 22:14:00 +0800

    28 (not understand the essence of KMP)

---
+++ 1-100/28. Implement strStr().java
@@ -0,0 +1,14 @@
+// 1.O(m*n)
+class Solution {
+    public int strStr(String haystack, String needle) {
+        for (int i = 0; ; i++) {
+            for (int j = 0; ; j++) {
+                if (j == needle.length()) return i;
+                if (i + j == haystack.length()) return -1;
+                if (needle.charAt(j) != haystack.charAt(i + j)) break;
+            }
+        }
+    }
+}
+
+// 2.KMP - non-trivial - O(m+n)
