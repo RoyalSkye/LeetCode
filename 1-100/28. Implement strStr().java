06f628ef741d6941f047d784af6a04094189a481
royalskye <a19970417b@qq.com>
2019-02-26 15:04:04 +0800

    28 solved by KMP && 30(implementation to be continued)

--- 1-100/28. Implement strStr().java
+++ 1-100/28. Implement strStr().java
@@ -12,3 +12,42 @@ class Solution {
 }
 
 // 2.KMP - non-trivial - O(m+n)
+class Solution {
+    public int strStr(String haystack, String needle) {  //KMP-O(N+M)
+        if(needle.length() == 0) return 0;
+        if(haystack.length() == 0 || haystack.length() < needle.length()) return -1;
+        char[] hh = haystack.toCharArray();
+        char[] mm = needle.toCharArray();
+        int[] nextArr = getNextArr(mm);
+        int hi = 0, mi = 0;
+        while(hi < hh.length && mi < mm.length){
+            if(hh[hi] == mm[mi]){
+                hi++;
+                mi++;
+            }else if(nextArr[mi] == -1){  //nextArr[mi] == 0 means compare hh[hi] with the start of needle
+                hi++;
+            }else{
+                mi = nextArr[mi];
+            }
+        }
+        return mi == mm.length ? hi - mi : -1;
+    }
+    
+    private int[] getNextArr(char[] match){
+        if(match.length == 1) return new int[]{-1};
+        int[] next = new int[match.length];
+        next[0] = -1;
+        next[1] = 0;
+        int pos = 2, cn = 0;
+        while(pos < match.length){
+            if(match[pos-1] == match[cn]){
+                next[pos++] = ++cn;
+            }else if(cn > 0){
+                cn = next[cn];
+            }else{
+                next[pos++] = 0;
+            }
+        }
+        return next;
+    }
+}
